5fc8aad6a197b7a1415595dd5d7c39403e7723fb
Jason Ozulumba <cjozulumba@gmail.com>
2020-10-31 23:25:10 -0400

    Got names to sort alphabetically

--- sort-it-out/src/components/EmployeeWebsite.jsx
+++ sort-it-out/src/components/EmployeeWebsite.jsx
@@ -4,13 +4,61 @@ import seed from "../seed.json";
 
 class EmployeeWebsite extends Component {
   state = {
-    result: {},
+    result: [],
     search: "",
     seed,
+    nameCounter: 0,
   };
 
+  componentDidMount() {
+    this.setState({
+      result: seed,
+    });
+  }
+
+  nameSort = () => {
+    if (this.state.nameCounter === 0) {
+      this.handleSort("Alpha");
+      this.setState({ nameCounter: 1 });
+    } else {
+      this.handleSort("Beta");
+      this.setState({ nameCounter: 0 });
+    }
+  }
+
   handleSort = (method) => {
-      
+    let sortedArray;
+    if (method === "Alpha") {
+      sortedArray = seed.sort((a, b) => {
+        let fa = a.name.first.toLowerCase(),
+          fb = b.name.first.toLowerCase();
+
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+          return 1;
+        }
+        return 0;
+      });
+
+      this.setState({ result: sortedArray });
+    } else if (method === "Beta") {
+      sortedArray = seed.sort((a, b) => {
+        let fa = a.name.first.toLowerCase(),
+          fb = b.name.first.toLowerCase();
+
+        if (fa > fb) {
+          return -1;
+        }
+        if (fa < fb) {
+          return 1;
+        }
+        return 0;
+      });
+
+      this.setState({ result: sortedArray });
+    }
   };
 
   handleInputSearch = (event) => {};
@@ -18,26 +66,40 @@ class EmployeeWebsite extends Component {
   render() {
     return (
       <main className='container'>
+        <section className='jumbotron jumbotron-fluid text-center'>
+          <h2 className='display-4'>Simple Sorting Table</h2>
+          <p className='lead'> Just a simple sorting table with seed data</p>
+        </section>
         <div className='row'>
-          <section className="col-lg-12">
-            <table id='employees' className="table table-striped table-bordered table-md" cellSpacing="0" width="100%">
+          <br />
+          <br />
+          <br />
+          <section className='col-lg-8'>
+            <table
+              id='employees'
+              className='table table-striped table-bordered table-md align-content-center'
+              cellSpacing='0'
+              width='100%'
+            >
               <thead>
-              <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-              </tr>
+                <tr>
+                  <th onClick={this.nameSort} >Name</th>
+                  <th scope='col'>Email</th>
+                  <th scope='col'>Phone</th>
+                </tr>
               </thead>
-              {this.state.seed.map((person) => (
-                <Employee
-                  id={person.id}
-                  key={person.id}
-                  fullname={`${person.name.first} ${person.name.last}`}
-                  email={person.email}
-                  image={person.picture.medium}
-                  phone={person.phone}
-                />
-              ))}
+              <tbody>
+                {this.state.result.map((person) => (
+                  <Employee
+                    id={person.id}
+                    key={person.id}
+                    fullname={`${person.name.first} ${person.name.last}`}
+                    email={person.email}
+                    image={person.picture.medium}
+                    phone={person.phone}
+                  />
+                ))}
+              </tbody>
             </table>
           </section>
         </div>
